d346a5c0da2551cf1e109697605f02438f8e7db2
Ibrahim Younes Zaidi <zaidibrahim2003@Gmail.com>
2025-09-23 16:44:32 +0100

    fixed things

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -23,7 +23,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      process.env.CLIENT_URL || "",
+      process.env.BASE_API_URL || "",
     ],
     credentials: true,
   })
